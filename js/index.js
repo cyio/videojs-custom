573b73f5c2962b9fcef382fec17e08f8fa529ea9
cyio <icaner@qq.com>
2019-04-24 14:52:50 +0800

    feat: resolution switch

--- js/index.js
+++ js/index.js
@@ -12,15 +12,36 @@ const options = {
       src: '//vjs.zencdn.net/v/oceans.mp4',
       type: 'video/mp4'
     }
-  ]
+  ],
+  playbackRates: [ 0.75, 1.0, 1.2, 1.5 ],
+  plugins: {
+    videoJsResolutionSwitcher: {
+      default: '480',
+      dynamicLabel: true
+    }
+  },
+  controlBar: {
+    children: [
+      'playToggle',
+      'currentTimeDisplay',
+      'timeDivider',
+      'durationDisplay',
+      'progressControl',
+      'volumePanel',
+      'playbackRateMenuButton',
+      'fullscreenToggle'
+    ],
+    volumePanel: {inline: false}
+  },
   // autoplay: true,
   // muted: true,
 }
 const videojs = window.videojs
-const player = videojs('player', options, function onPlayerReady() {
+const player = videojs('player', options)
+player.on('ready', function() {
   videojs.log('Your player is ready!')
 
-  this.hotkeys({
+  player.hotkeys({
     volumeStep: 0.1,
     seekStep: 5,
     enableModifiersForNumbers: false
@@ -28,11 +49,29 @@ const player = videojs('player', options, function onPlayerReady() {
 
   // In this context, `this` is the player that was created by Video.js.
   // this.play();
+})
 
-  // How about an event listener?
-  this.on('ended', function() {
-    videojs.log('Awww...over so soon?!')
-  })
+// How about an event listener?
+player.on('ended', function() {
+  videojs.log('Awww...over so soon?!')
+})
+
+player.updateSrc([
+  {
+    src: 'https://vjs.zencdn.net/v/oceans.mp4',
+    type: 'video/mp4',
+    res: 480,
+    label: '标清'
+  },
+  {
+    src: 'https://vjs.zencdn.net/v/oceans.mp4',
+    type: 'video/mp4',
+    res: 720,
+    label: '高清'
+  },
+])
+player.on('resolutionchange', function(){
+  console.info('Source changed to %s', player.src())
 })
 
 let duration = null
