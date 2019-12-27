4227780a2c12c4a953e64f21742242c40900ac3d
cyio <icaner@qq.com>
2019-12-27 14:42:27 +0800

    fix buffer compute

--- js/log.js
+++ js/log.js
@@ -1,6 +1,10 @@
 // 卡顿缓冲时长
-function myLog(video) {
+function bufferLog(video) {
   video._buffer_times = 0;
+  let waitingTimeout = null
+  let waitingDuration = 0
+  const maxWaitingDuration = 60
+
 
   // 视频卡顿
   const logBuffer = (video) => {
@@ -13,6 +17,9 @@ function myLog(video) {
 
     if (video._waiting_time && video._playing_time) {
       const bufferCost = video._playing_time - video._waiting_time;
+      if (bufferCost <= 0 || bufferCost > 1000 * 60) {
+        console.log('buf', bufferCost)
+      }
       if (bufferCost > 4000) {
         console.log('Buffer', [(bufferCost / 1000).toFixed(1) + 's']);
         console.log({
@@ -33,16 +40,45 @@ function myLog(video) {
       const key = `_${eventName}_time`;
       video[key] = Date.now();
 
-      // 在 playing 时记录卡顿
+      // 在 playing 时记录卡顿，此法可记录绝大多数卡顿，但如果 playing 未触发呢？超时就提示重试了
       if (eventName === 'playing') {
         logBuffer(video);
+
+        if (waitingTimeout) {
+          clearInterval(waitingTimeout)
+          waitingDuration = 0
+        }
       }
 
       // 播放结束重置卡顿计数器
       if (eventName === 'ended') {
         video._buffer_times = 0;
       }
+
+      // 判断持续卡顿
+      if (eventName === 'waiting') {
+        waitingTimeout = setInterval(() => {
+          waitingDuration += 3
+          if (waitingDuration > maxWaitingDuration) {
+            clearInterval(waitingTimeout)
+            waitingDuration = 0
+            if (confirm('刷新')) {
+              video.src = video.src
+              video.play()
+            }
+          }
+        }, 3000)
+      }
     });
+  });
+
+  // 卡顿计算，有两种情况需要排除，1. waiting 时往前前跳触发 playing 2. 往后跳转
+  // 解决办法：1. 有共性的 pause 事件 2. 也可记录事件发生时的播放相对时间，判断时间连续
+  // 这里采用方法 1，发生 pause 时重置计算
+  video.addEventListener('pause', () => {
+    if (video._waiting_time) {
+      video._waiting_time = null;
+    }
   });
 
   // 断网不计算 buffer
@@ -77,11 +113,11 @@ function myLog(video) {
 function eventLog(video) {
   [
     'loadstart',
-    'progress',
-    'suspend',
+    // 'progress',
+    // 'suspend',
     'abort',
     'error',
-    'emptied',
+    'emptied', // 已载入时，调用`load()`方法重载时触发
     'stalled',
     'loadedmetadata',
     'loadeddata',
@@ -96,7 +132,7 @@ function eventLog(video) {
     // 'timeupdate',
     'play',
     'pause',
-    'ratechange',
+    'ratechange', // playbackrate
     'resize',
     'volumechange',
   ].forEach(eventName => {
